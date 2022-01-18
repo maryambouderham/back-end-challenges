d2ef30f6b04c85f87d35e359bc6977537bf737e9
maryambouderham <maryam.bouderham@gmail.com>
2022-01-18 10:15:51 +0100

    startup project

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const { API_URL } = require("./config/api")
+const { db } = require("./config/mysql")
+const cors = require("cors")
+const bp = require("body-parser")
+// const { verifyEmail,
+//         register,
+//     } = require("./api/auth")
+
+
+//Connect
+db.connect((err) => {
+    if (err) throw err
+    console.log("Mysql connected...")
+})
+
+const app = express()
+
+//body could have diff types 
+app.use(bp.urlencoded({ extended: true }))
+//looks at requests where the Content-Type: application/json (Header)
+app.use(bp.json())
+//appliquer le cors comme middle ware 
+app.use(cors())
+
+
+app.listen('9000', () => {
+    console.log('Server started on port 9000 😇');
+})
+
+// //register
+// app.post(`/${API_URL.auth}/register`, register)
+
+// //verify email after register
+// app.post(`/${API_URL.auth}/verify-email`, verifyEmail)
